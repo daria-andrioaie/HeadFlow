4f769e9c13a8144869ef9fa0cd2fb4f7709ad4c0
Andrioaie Daria Maria <daria.andrioaie@gmail.com>
2023-05-16 22:00:00 +0300

    therapist - upload profile picture

--- HeadFlow/HeadFlow/Flows/Main - Therapist/Profile/TherapistProfileView.swift
+++ HeadFlow/HeadFlow/Flows/Main - Therapist/Profile/TherapistProfileView.swift
@@ -12,17 +12,148 @@ struct TherapistProfile {
         @ObservedObject var viewModel: ViewModel
         
         var body: some View {
-            ContainerWithNavigationBar(title: Texts.GeneralProfile.profileNavbarTitle, leftButtonAction: {
-                viewModel.navigationAction(.goBack)
-            }) {
-                VStack {
-                    Spacer()
-                    Buttons.LogoutButton {
-                        viewModel.logout()
-                    }
+            VStack {
+                NavigationBar(title: Texts.GeneralProfile.profileNavbarTitle, leftButtonAction: { viewModel.navigationAction(.goBack) } ) {
+                    logoutButton
                 }
-                .toastDisplay(isPresented: $viewModel.isConfirmationMessagePresented, message: viewModel.confirmationMessage)
-                .padding(.all, 24)
+                mainContent
+            }
+            .fillBackground()
+            .sheet(isPresented: $viewModel.isImagePickerShown) {
+                ImagePickerView(image: $viewModel.newProfileImage)
+            }
+            .onChange(of: viewModel.newProfileImage) { _ in
+                viewModel.uploadImage()
+            }
+            .toastDisplay(isPresented: $viewModel.isConfirmationMessagePresented, message: viewModel.confirmationMessage)
+        }
+        
+        var mainContent: some View {
+            VStack {
+                profilePictureView
+                    .padding(.bottom, 20)
+                inputFields
+                    .padding(.bottom, 30)
+
+                Spacer()
+                saveButton
+            }
+            .activityIndicator(viewModel.isLoading)
+            .errorDisplay(error: $viewModel.apiError)
+            .padding(.all, 24)
+        }
+        
+        var logoutButton: some View {
+            Button {
+                viewModel.logout()
+            } label: {
+                Image(.logoutIcon)
+                    .renderingMode(.template)
+                    .foregroundColor(.danubeBlue)
+            }
+            .buttonStyle(.plain)
+        }
+        
+        var profilePictureView: some View {
+            ZStack(alignment: .bottomTrailing) {
+                if let chosenImage = viewModel.newProfileImage {
+                    Image(uiImage: chosenImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 160, height: 160)
+                        .clipShape(Circle())
+                } else {
+                    HFAsyncImage(url: viewModel.currentProfileImage, placeholderImage: .placeholderImage)
+                        .frame(width: 160, height: 160)
+                        .clipShape(Circle())
+                }
+                
+                editProfilePicureButton
+            }
+        }
+        
+        var editProfilePicureButton: some View {
+            Button {
+                viewModel.isImagePickerShown = true
+            } label: {
+                Image(systemName: "pencil")
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20)
+                    .foregroundColor(.danubeBlue)
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.white)
+                        .shadow(radius: 10, x: 5, y: 5))
+            }
+            .buttonStyle(.plain)
+        }
+        
+        var inputFields: some View {
+            VStack(spacing: 16) {
+                if !viewModel.phoneNumber.isEmpty {
+                    phoneNumberField
+                }
+                
+                firstNameInputField
+                lastNameInputField
+                emailInputField
+            }
+        }
+        
+        var firstNameInputField: some View {
+            VStack(alignment: .leading, spacing: 6) {
+                Text("First name")
+                    .font(.Main.regular(size: 18))
+                    .foregroundColor(.danubeBlue.opacity(0.8))
+                
+                CustomTextField(inputText: $viewModel.firstName, placeholder: Texts.Register.firstNameInputPlaceholder)
+                    .font(.Main.light(size: 16))
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        
+        var lastNameInputField: some View {
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Last name")
+                    .font(.Main.regular(size: 18))
+                    .foregroundColor(.danubeBlue.opacity(0.8))
+                CustomTextField(inputText: $viewModel.lastName, placeholder: Texts.Register.lastNameInputPlaceholder)
+                    .font(.Main.light(size: 16))
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        
+        var phoneNumberField: some View {
+            HStack {
+                Image(systemName: "phone")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20)
+                    .foregroundColor(.danubeBlue.opacity(0.8))
+                
+                Text(viewModel.phoneNumber)
+                    .font(.Main.light(size: 20))
+                    .foregroundColor(.danubeBlue)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        
+        var emailInputField: some View {
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Email")
+                    .font(.Main.regular(size: 18))
+                    .foregroundColor(.danubeBlue.opacity(0.8))
+                CustomTextField(inputText: $viewModel.email, placeholder: Texts.Register.emailInputPlaceholder)
+                    .font(.Main.light(size: 16))
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        
+        var saveButton: some View {
+            Buttons.FilledButton(title: Texts.GeneralProfile.saveButtonLabel, rightIcon: .checkmarkIcon, size: .small, width: 105) {
+                viewModel.updateProfile()
             }
         }
     }
--- HeadFlow/HeadFlow/Flows/Main - Therapist/Profile/TherapistProfileViewModel.swift
+++ HeadFlow/HeadFlow/Flows/Main - Therapist/Profile/TherapistProfileViewModel.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import UIKit
 
 extension TherapistProfile {
     class ViewModel: ObservableObject {
+        @Published var newProfileImage: UIImage?
+        @Published var currentProfileImage: URL?
+        @Published var firstName: String = ""
+        @Published var lastName: String = ""
+        @Published var email: String = ""
+        @Published var phoneNumber: String = ""
+        
+        @Published var isImagePickerShown: Bool = false
+        @Published var isLoading: Bool = false
         @Published var apiError: Error?
         @Published var isConfirmationMessagePresented: Bool = false
         var confirmationMessage: String = ""
@@ -19,6 +29,7 @@ extension TherapistProfile {
         init(authenticationService: AuthenticationServiceProtocol, navigationAction: @escaping (ProfileNavigationType) -> Void) {
             self.authenticationService = authenticationService
             self.navigationAction = navigationAction
+            populateInputFields()
         }
         
         func logout() {
@@ -38,6 +49,46 @@ extension TherapistProfile {
             }
         }
         
+        func updateProfile() {
+            Task(priority: .userInitiated) { @MainActor in
+                await authenticationService.updateProfile(firstName: firstName, lastName: lastName, email: email) { [weak self] result in
+                    switch result {
+                    case .success(let user):
+                        self?.isConfirmationMessagePresented = true
+                        self?.confirmationMessage = "Your profile was updated."
+                        Session.shared.saveCurrentUser(user: user, token: Session.shared.accessToken!)
+                    case .failure(let error):
+                        self?.apiError = Errors.CustomError(error.message)
+                    }
+                }
+            }
+        }
+        
+        func uploadImage() {
+            Task(priority: .userInitiated) { @MainActor in
+                guard let newProfileImage else {
+                    return
+                }
+                await authenticationService.updateProfilePicture(newProfileImage) { [weak self] result in
+                    switch result {
+                    case .success(let user):
+                        Session.shared.saveCurrentUser(user: user, token: Session.shared.accessToken!)
+                    case .failure(let error):
+                        self?.apiError = Errors.CustomError(error.message)
+                    }
+                }
+            }
+        }
+        
+        private func populateInputFields() {
+            if let user = Session.shared.currentUser {
+                firstName = user.firstName
+                lastName = user.lastName
+                email = user.email
+                currentProfileImage = user.profilePicture
+                phoneNumber = user.phoneNumber ?? ""
+            }
+        }
     }
     
     enum ProfileNavigationType {
